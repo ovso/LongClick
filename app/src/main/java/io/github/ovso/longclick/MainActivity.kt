21898da77f69f1a93baf5145d6abf8e8c1321b61
jaeho <ovsoce@gmail.com>
2019-09-21 01:08:14 +0900

    Implement Longclick

--- app/src/main/java/io/github/ovso/longclick/MainActivity.kt
+++ app/src/main/java/io/github/ovso/longclick/MainActivity.kt
@@ -3,20 +3,38 @@ package io.github.ovso.longclick
 import android.os.Bundle
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
+    private val compositeDisposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn.setOnTouchListener { view, motionEvent ->
             when (motionEvent.action) {
-                MotionEvent.ACTION_DOWN -> println("ACTION_DOWN")
-                MotionEvent.ACTION_UP -> println("ACTION_UP")
+                MotionEvent.ACTION_DOWN -> handleTimer()
+                MotionEvent.ACTION_UP -> compositeDisposable.clear()
             }
 
             true
         }
+    }
+
+    private fun handleTimer() {
+        compositeDisposable.add(
+            Observable.timer(
+                3,
+                TimeUnit.SECONDS
+            ).subscribeOn(Schedulers.io())
+                .subscribe {
+                    println("Long click!!! 3 seconds")
+                }
+        );
+
     }
 }
