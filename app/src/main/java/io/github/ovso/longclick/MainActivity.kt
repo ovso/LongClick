c54f920157a0a14ce29e9f66e660f76c3a4fe759
jaeho <ovsoce@gmail.com>
2019-09-21 00:59:15 +0900

    Set button touchevent

--- app/src/main/java/io/github/ovso/longclick/MainActivity.kt
+++ app/src/main/java/io/github/ovso/longclick/MainActivity.kt
@@ -1,12 +1,22 @@
 package io.github.ovso.longclick
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn.setOnTouchListener { view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> println("ACTION_DOWN")
+                MotionEvent.ACTION_UP -> println("ACTION_UP")
+            }
+
+            true
+        }
     }
 }
